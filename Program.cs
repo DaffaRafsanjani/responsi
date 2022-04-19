2f65ff93118ad6a4e47c3cd9f6cd3a1d2a9abbcc
DaffaRafsanjani <101723280+DaffaRafsanjani@users.noreply.github.com>
2022-04-19 16:37:30 +0700

    tugas responsi uts

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Responsi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawans karyawans = new karyawans(190302123, "Paijo", 3000000);
+            Karyawans karyawans = new Karyawans(190302124, "joni", 2000000);
+
+
+            Console.WriteLine("NIK \t Nama \t\t Gaji");
+            Console.WriteLine("-------------------------------");
+            karyawans.Karyawans();
+            karyawans.Karyawans();
+
+            Console.WriteLine(" Asyiiiiik kenaikan gaji 10!!");
+
+            Console.WritweLine("NIK \t Nama \t\t Gaji");
+            Console.WriteLine("-------------------------------");
+            karyawan.naikGaji();
+            karyawan2.naikGaji();
+
+            Console.ReadKey();
+        }
+    }
+}
